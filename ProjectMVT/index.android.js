e57ab4c3f1964986bfdb26a9459dc3140db3bc69
guobg <guobg1987@163.com>
2017-05-07 17:09:05 +0800

    Create project mindvation

---
+++ ProjectMVT/index.android.js
@@ -0,0 +1,13 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry
+} from 'react-native';
+import setup from './js/setup';
+
+AppRegistry.registerComponent('ProjectMVT', () => setup);//整个react-native应用可以看作一个包裹整个项目的组件，而AppRegistry注册的组件作为RN应用的入口函数。
---
+++ ProjectMVT/js/setup.js
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    Navigator
+} from 'react-native';
+
+export default class setup extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Logon</Text>
+            </View>
+
+        );
+    }
+}
